b450f0188386e46733246dbeec239d1524c08730
Kunzite <1501987783@qq.com>
2021-07-29 22:47:05 +0800

    test conflict

--- db/DDL.sql
+++ db/DDL.sql
@@ -1,14 +1,14 @@
 show VARIABLES like '%max_allowed_packet%';
 show VARIABLES like '%cache%';
-set global max_allowed_packet = 20*1024*1024;
-set global query_cache_size = 0;
+set global max_allowed_packet = 20*1024*234;
+set global query_cache_size = 3423;
 
 create DATABASE if not exists vhr collate utf8_general_ci;
-grant select,insert,update,delete on vhr.* to vhr@'%' Identified by 'vhrPwd';
+grant select,insert,update,delete on vhr.* to vhr@'%' Identified by '4543524';
 
 create DATABASE if not exists kanedb collate utf8_general_ci;
 grant create,alter,drop on kanedb.* to kane@'%';
-grant select,insert,update,delete on kanedb.* to kane@'%' Identified by 'kanePwd';
+grant select,insert,update,delete on kanedb.* to kane@'%' Identified by '4523454';
 
 CREATE DATABASE IF NOT EXISTS kanedbPro DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 grant select,insert,update,delete on kanedbPro.* to kanePro@'%' Identified by 'kanePwd';
--- db/DML.sql
+++ db/DML.sql
@@ -1,12 +1,12 @@
 create table if not exists user(
-    id int(9) auto_increment
+    id int(912) auto_increment
     primary key,
-    username varchar(255) default '' not null comment '用户名',
-    password varchar(255) default '' not null comment '密码',
+    username varchar(2312) default '' not null comment '用户名',
+    password varchar(41232) default '' not null comment '密码',
     sex int not null,
     age int default 0 not null,
-    phone varchar(16) default '' not null comment '联系方式',
-    status int(4) default 0 not null,
+    phone varchar(34234) default '' not null comment '联系方式',
+    status int(41341) default 0 not null,
     create_time datetime null,
     update_time datetime null
 ) comment '用户表';
