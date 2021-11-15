4a6aa043c0d15881e7f7a3acf6bc30f424d71d71
Kunzite <1501987783@qq.com>
2021-11-15 22:47:12 +0800

    add rabbitmq exchange

--- spring-example-mq/src/main/java/com/wyb/mq/rabbit/mq/base/Producer.java
+++ spring-example-mq/src/main/java/com/wyb/mq/rabbit/mq/base/Producer.java
@@ -5,11 +5,11 @@
  */
 package com.wyb.mq.rabbit.mq.base;
 
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
-
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author Marcher丶
@@ -22,13 +22,23 @@ public class Producer {
     public static void main(String[] args) throws IOException, TimeoutException {
         Connection connection = ConnectionUtil.getConnection();
         Channel channel = connection.createChannel();
+        /**
+         * 以前讲过说我们的消息会发送的exchange上，
+         * 但是在这里我们没有指定交换机?那我们的消息发送到哪里了？？？？
+         * The default exchange is implicitly bound to every queue, with a routing key equal to the queue name.
+         * It is not possible to explicitly bind to, or unbind from the default exchange. It also cannot be deleted.
+         * 说明:加入我们消息发送的时候没有指定具体的交换机的话，那么就会发送到rabbimtq指定默认的交换机上，
+         * 那么该交换机就会去根据routing_key 查找对应的queueName 然后发送的该队列上.
+         *
+         */
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
-        // 消息内容
-        for (int i = 0; i < 10;i ++) {
+        // 通过channel发送消息
+        for (int i = 0; i < 5; i++) {
             String message = "Hello World!" + i;
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
         }
+
         channel.close();
         connection.close();
     }
---
+++ spring-example-mq/src/main/java/com/wyb/mq/rabbit/mq/base/topic_exchange/TopicExchangeConsumer.java
@@ -0,0 +1,54 @@
+package com.wyb.mq.rabbit.mq.base.topic_exchange;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class TopicExchangeConsumer {
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        //创建连接工厂
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("192.168.159.8");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("tuling");
+        connectionFactory.setUsername("smlz");
+        connectionFactory.setPassword("smlz");
+
+        //创建连接
+        Connection connection = connectionFactory.newConnection();
+
+        //创建channel
+        Channel channel = connection.createChannel();
+
+        //声明交换机
+        String exchangeName = "tuling.topicexchange";
+        String exchangeType = "topic";
+        channel.exchangeDeclare(exchangeName,exchangeType,true,true,null);
+
+        //声明队列
+        String quequName = "tuling.topic.queue";
+        channel.queueDeclare(quequName,true,false,false,null);
+
+        //声明绑定关系
+        String bingdingStr = "tuling.*";
+        channel.queueBind(quequName,exchangeName,bingdingStr);
+
+        //声明一个消费者
+//        QueueingConsumer queueingConsumer = new QueueingConsumer(channel);
+
+        //开始消费
+        /**
+         * 开始消费
+         */
+        channel.basicConsume(quequName, true, new DefaultConsumer(channel) {
+
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String reciverMessage = new String(body);
+                System.out.println("消费消息:-----" + reciverMessage);
+            }
+        });
+    }
+}
