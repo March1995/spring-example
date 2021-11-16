41753cf4ef67e81ac6eb58e76e495b238344b4b8
wangyb <1501987783@qq.com>
2021-11-16 17:06:01 +0800

    add rabbitmq 分布式事务解决方案（消息可靠性投递之定时任务版本）

--- spring-example-mq/src/main/java/com/wyb/mq/rabbit/controller/OrderController.java
+++ spring-example-mq/src/main/java/com/wyb/mq/rabbit/controller/OrderController.java
@@ -5,7 +5,7 @@ import com.wyb.mq.entity.OrderInfo;
 import com.wyb.mq.service.IOrderInfoService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
@@ -17,7 +17,7 @@ public class OrderController {
     @Autowired
     private IOrderInfoService orderInfoService;
 
-    @RequestMapping("/saveOrder")
+    @GetMapping("/saveOrder")
     public String saveOrder() throws JsonProcessingException {
 
         OrderInfo orderInfo = new OrderInfo();
