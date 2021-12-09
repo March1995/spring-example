b9e735d71f4b33ef998dfa065380ed86dc9fe256
Kunzite <1501987783@qq.com>
2021-12-09 22:24:37 +0800

    update

--- spring-example-cache/src/main/java/com/wyb/cache/controller/CacheController.java
+++ spring-example-cache/src/main/java/com/wyb/cache/controller/CacheController.java
@@ -1,16 +1,14 @@
 package com.wyb.cache.controller;
 
-import javax.annotation.Resource;
-
+import com.wyb.cache.dao.model.UserDo;
+import com.wyb.cache.service.CacheService;
+import com.wyb.cache.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wyb.cache.dao.model.UserDo;
-import com.wyb.cache.service.CacheService;
-import com.wyb.cache.service.UserService;
-
-import lombok.extern.slf4j.Slf4j;
+import javax.annotation.Resource;
 
 /**
  * Description:
@@ -29,12 +27,23 @@ public class CacheController {
     @Resource
     private CacheService redisService;
 
-    @GetMapping("/add")
+    @GetMapping("/addById")
     public UserDo addById() {
         UserDo userDo = userService.getById(1);
         redisService.putCache(String.valueOf(userDo.getId()), userDo);
         userDo = (UserDo) redisService.getCache(userDo.getId().toString());
         return userDo;
+    }
+
+    @GetMapping("/add")
+    public Object add(String key, String value, Integer expire) {
+        if (null != expire) {
+            redisService.putCache(key, value, expire);
+        }
+        else {
+            redisService.putCache(key, value);
+        }
+        return redisService.getCache(key);
     }
 
     @GetMapping("/get")
