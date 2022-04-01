95bb676fbc5e325bafe1062830d42704051c8b6c
Marcher <1501987783@qq.com>
2022-04-01 21:57:18 +0800

    ReturnFinallyTest

--- spring-example-test/src/main/java/com/wyb/test/java/exception/ReturnFinallyTest.java
+++ spring-example-test/src/main/java/com/wyb/test/java/exception/ReturnFinallyTest.java
@@ -2,6 +2,10 @@ package com.wyb.test.java.exception;
 
 /**
  * try.catch 中的return在执行完finally的代码后再执行，若finally有return不执行之前的  return返回的值为各模块的值 互不影响
+ * <p>
+ * 1、finally中的代码总会被执行(用一种情况finally中的代码不会执行，那就是在try{}块中包含遇到System.exit(0))。
+ * 2、当try、catch中有return时，也会执行finally。return的时候，要注意返回值的类型，是否受到finally中代码的影响。如果是基本数据类型，返回的是try、catch中的值，是引用数据类型时，返回finally中的值
+ * 3、finally中有return时，会直接在finally中退出，导致try、catch中的return失效。
  *
  * @author Kunzite
  */
