e4dc1d1686e26f4b099db76f14484be7f8ae0f0d
Marcher <1501987783@qq.com>
2022-11-10 22:32:55 +0800

    值传递和引用传递

--- spring-example-test/src/main/java/com/wyb/test/java/common/BaseEntity.java
+++ spring-example-test/src/main/java/com/wyb/test/java/common/BaseEntity.java
@@ -1,6 +1,9 @@
 package com.wyb.test.java.common;
 
 import com.alibaba.fastjson.JSONObject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -8,11 +11,16 @@ import java.util.Date;
 /**
  * @author Kunzite
  */
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class BaseEntity implements Serializable {
 
     private static final long serialVersionUID = -1618357536992740779L;
 
     protected Long id;
+
+    protected String name;
 
     protected Integer isDelete;
 
@@ -24,42 +32,30 @@ public class BaseEntity implements Serializable {
         this.id = id;
     }
 
-    public BaseEntity() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getIsDelete() {
-        return isDelete;
-    }
-
-    public void setIsDelete(Integer isDelete) {
-        this.isDelete = isDelete;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
+    public BaseEntity(String name) {
+        this.name = name;
     }
 
     public static BaseEntity parseConfig(String str) {
         return JSONObject.parseObject(str, BaseEntity.class);
     }
+
+
+
+    public static void main(String[] args) {
+        BaseEntity xiaoZhang = new BaseEntity("小张");
+        BaseEntity xiaoLi = new BaseEntity("小李");
+        swap(xiaoZhang, xiaoLi);
+        System.out.println("xiaoZhang:" + xiaoZhang.getName());
+        System.out.println("xiaoLi:" + xiaoLi.getName());
+    }
+
+    public static void swap(BaseEntity person1, BaseEntity person2) {
+        BaseEntity temp = person1;
+        person1 = person2;
+        person2 = temp;
+        System.out.println("person1:" + person1.getName());
+        System.out.println("person2:" + person2.getName());
+    }
+
 }
