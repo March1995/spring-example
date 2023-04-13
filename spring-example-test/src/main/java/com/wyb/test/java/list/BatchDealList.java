b5a62a0a1220458d90ed8f6a44d3101935acf73b
wangyb <1501987783@qq.com>
2023-04-13 17:22:35 +0800

    test

--- spring-example-test/src/main/java/com/wyb/test/java/list/BatchDealList.java
+++ spring-example-test/src/main/java/com/wyb/test/java/list/BatchDealList.java
@@ -3,7 +3,6 @@ package com.wyb.test.java.list;
 import com.wyb.test.java.common.BaseEntity;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -19,6 +18,7 @@ public class BatchDealList {
 
 
     public static void main(String[] args) {
+
         int batchSize = 100;
 
         List<BaseEntity> lists = new ArrayList<>();
