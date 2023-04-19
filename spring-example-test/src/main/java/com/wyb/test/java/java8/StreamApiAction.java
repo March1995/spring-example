350deee4529f0143e19021a51770e54fd3a5f8a4
wangyingbo <1501987783@qq.com>
2023-04-19 14:08:41 +0800

    foreach 循环 每次传入的是新的list

--- spring-example-test/src/main/java/com/wyb/test/java/java8/StreamApiAction.java
+++ spring-example-test/src/main/java/com/wyb/test/java/java8/StreamApiAction.java
@@ -29,23 +29,45 @@ public class StreamApiAction {
 //        flatMap(allList);
         // 留下偶数
 //        filter(list);
-        List<Integer> list3 = Arrays.asList(1, 2, 2, 2, 5);
+
+//        List<Integer> list3 = Arrays.asList(1, 2, 2, 2, 5);
 //        distinct(list3);
-        list3 = list3.stream().filter(distinctByKey(v -> v)).collect(Collectors.toList());
-        list3.forEach(System.out::println);
+//        list3 = list3.stream().filter(distinctByKey(v -> v)).collect(Collectors.toList());
+//        list3.forEach(System.out::println);
+
 //        sorted(list);
+
 //        peek(list);
+
 //        limit(list);
+
 //        skip(list);
+
 //        parallel(list);
+
 //        sequential(list);
+
 //        unordered(list);
+
 //        Long[] aa = list.stream().map(value -> value * value).toArray(Long[]::new);
 //        System.out.println(list.stream().reduce(0, Integer::sum));
 //        Optional<Integer> optional = list.stream().reduce(Integer::sum);
 //        System.out.println(optional.get());
 
         Integer num = list.stream().mapToInt(Integer::intValue).sum();
+        // 每次循环 传入的list2 都是新的对象 所以不会出现并发修改异常
+        list.forEach(v -> testParallel(list2));
+        list2.forEach(System.out::println);
+    }
+
+
+    /**
+     * 并发修改
+     * @param list
+     */
+    private static List<Integer> testParallel(List<Integer> list) {
+        list = list.stream().filter(v -> v % 2 == 0).collect(Collectors.toList());
+        return list;
     }
 
     private static void map(List<Integer> list) {
